82ea85ea7dcaafcf2535f26cd8106e9234d4e215
Keheira <keheirah@gmail.com>
2017-05-26 04:20:35 -0400

    Step 2

---
+++ Ledger/app/src/main/java/keheira/h/ledger/Payee.java
@@ -0,0 +1,15 @@
+package keheira.h.ledger;
+
+/**
+ * Created by Keheira on 5/26/2017.
+ */
+
+public class Payee {
+    private final String name;
+    private final double amount;
+
+    public Payee(String n, double a){
+        name = n;
+        amount = a;
+    }
+}
